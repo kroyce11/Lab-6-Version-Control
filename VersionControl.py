4a5a50f9db9915e1639aeb7fd0ddedae9062aa5f
surfe <surfer.brock1@gmail.com>
2023-10-31 20:49:46 -0400

    added decode function and logic for menu option 2

--- VersionControl.py
+++ VersionControl.py
@@ -1,4 +1,3 @@
-x=0
 
 def encode(password):
     encoded_password = ""
@@ -7,6 +6,15 @@ def encode(password):
         encoded_password += str(shifted_digit)
     print('Your password has been encoded and stored! ')
     return encoded_password
+
+
+def decode(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        shifted_digit = (int(digit) - 3) % 10
+        decoded_password += str(shifted_digit)
+    return decoded_password
+
 
 def print_menu():
     print('Menu')
@@ -17,16 +25,16 @@ def print_menu():
     print()
 
 
-
-
-
-while x == 0:
-    print_menu()
-    user_input = int(input('Please enter an option: '))
-    if user_input == 3:
-        x = 1
-    elif user_input == 1:
-        password = input('Please enter your password to encode: ')
-        encoded_password = encode(password)
-    elif user_input == 2:
-        decode(encoded_password)
+def main():
+    x = 0
+    while x == 0:
+        print_menu()
+        user_input = int(input('Please enter an option: '))
+        if user_input == 3:
+            x = 1
+        elif user_input == 1:
+            password = input('Please enter your password to encode: ')
+            encoded_password = encode(password)
+        elif user_input == 2:
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
